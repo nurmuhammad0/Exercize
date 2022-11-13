15368a0100071bb89ec19b2d8eeff85e64b82391
Muhammad <nurmirzayevmuhammad6@gmail.com>
2022-11-13 16:48:13 +0500

    hometask

---
+++ index.js
@@ -0,0 +1,67 @@
+// ===============
+// prefix and postfix
+
+// let a = 1;
+// let b = 1;
+
+// c = ++a;     //2
+// d = b++;     //1
+// console.log(c,d);
+
+// ==========
+// Assigment result
+
+// let a = 2;
+// let x = 1 + (a*=2)  //1+4=5
+
+// console.log(x);
+
+// console.log(""+ 1 + 0); // typeof string result 10
+// console.log("" - 1 + 0); // NaN result -1
+
+// console.log(true + false); //result 1
+
+// console.log(6 / "3"); //typeof number result 2
+
+// console.log("2" * "3") //  result 6
+
+// console.log(4 + 5 + "px"); // type of string rsult 9px
+
+// console.log("4" - 2); //result 2
+
+// console.log("4px" - 2); // NaN
+
+// console.log("-9" + 5); //typeof string result -95
+
+// console.log("-9" - 5); //typeof number result -14
+
+// console.log("$" + 4 + 5); // typeof string result $45
+
+// console.log(null + 1);// result 1
+
+// console.log(undefined + 1); // result NaN
+
+// console.log("\t \n " - 2); // result -2
+
+// ============
+// Comprasions
+
+// console.log(5 > 4); // true
+
+// console.log("apple" > "pineapple" ); // false
+
+// console.log("2" > "12"); // true
+
+// console.log(undefined == null); // true
+
+// console.log(undefined === null ); //false
+
+// console.log(null == "\n0\n"); //false
+
+// console.log(null === +"\n0\n"); //false
+
+
+
+
+
+
