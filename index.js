5fab08feaa52c21a5105984acf3a4133d065f9d7
Muhammad <nurmirzayevmuhammad6@gmail.com>
2022-11-16 04:18:25 +0500

    if/else

---
+++ index.js
@@ -0,0 +1,38 @@
+//=========
+// if("0"){
+//     console.log("Hello");
+// }
+//==========
+// let a = "EcmaScript";
+// if(a === "EcmaScript"){
+//     console.log("You are right");
+// }
+// else{
+//     console.log("Don't you know? The right answer was EcmaScript");
+// }
+// ============
+// let a = 0;
+// if(a > 0){
+//     console.log("the value is greater than zero");
+// }
+// else if(a < 0){
+//     console.log("the value is less than zero");
+// }
+// else{
+//     console.log("the value equal zero");
+// }
+// =============
+// let a=4;
+// let b=1;
+
+// a+b<4?console.log("result='Below'"):console.log("result='Over'");
+
+// let login="Employee";
+// login == "Employee"?console.log("Hello"):login=="Director"?console.log("Greetings"):login==""?console.log("no login"):console.log("");;
+
+// let login="b";
+// let massage;
+// if (login =="a" )massage="hello";
+// else if(login =="b")massage="Director";
+// else massage="no passwords";
+// console.log(massage);
