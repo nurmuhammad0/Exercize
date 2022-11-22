4b85ad8d7e529271367ea29e0befdcd1de50b749
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-11-22 21:36:18 +0500

    object

---
+++ 2211/index.js
@@ -0,0 +1,28 @@
+// let user = {
+//     name:"John",
+//     surname:"Smith",
+
+// };
+
+
+// console.log(user);
+// ===========
+// let user = {
+//     name:"John",
+//     surname:"Smith",
+
+// };
+
+// user.name = "Pete"
+// console.log(user);
+
+// =============
+
+// let user = {
+//     name:"John",
+//     surname:"Smith",
+
+// };
+
+// delete user.name;
+// console.log(user);
