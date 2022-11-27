eff9bc818d9544608b5842e7e6ac6887662030bf
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-11-27 20:58:48 +0500

    functions

---
+++ 2711/index.js
@@ -0,0 +1,29 @@
+// function checkAge(age){
+//     if(age>=18){
+//         return true;
+//     }else{
+//         return "Did your parents allow you"
+//     }
+// }
+// console.log(checkAge(19));
+
+// function checkMin(a,b){
+//     if(a<b){
+//         return a;
+//     }
+//     else if(a>b){
+//         return b;
+//     }
+//     else{
+//         return a;
+//     }
+
+// }
+// console.log( checkMin(3,3));
+
+
+// function calcPow(x,n){
+//     return x ** n;
+// }
+// console.log(calcPow(3,2));
+
