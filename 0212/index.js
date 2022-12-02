c15ca692965566b3c0151d0594292ea898503313
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-12-02 06:08:38 +0500

    sum of array number

---
+++ 0212/index.js
@@ -0,0 +1,12 @@
+// sum of array numbers
+
+let arr=[1,"w",3,"s",7,"2",1]
+let sum=0;
+for(let i=0;i<arr.length;i++){
+    arr[i]=Number(arr[i])
+    if(!isNaN(arr[i])){
+        sum+=arr[i]
+    }
+}
+console.log(sum);
+
