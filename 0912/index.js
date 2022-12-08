f40020046fa6264c888db8a4a7b113fe718ea783
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-12-09 04:38:14 +0500

    dublicate

--- 0912/index.js
+++ 0912/index.js
@@ -1,8 +1,20 @@
+//@ to a without using replace method
+// let str="Webbr@in Ac@demy";
+// let newString=""
+// for(v of str){
+//     newString += v==="@"?"a":v
 
-let str="Webbr@in Ac@demy";
-let newString=""
-for(v of str){
+// }
+// console.log(newString);
 
-        newString += v==="@"?"a":v
-}
-console.log(newString);
+// const dublicateStr=(str)=>{
+// let res = new Set(str.split(""));
+// let resStr="";
+// for(i of res){
+//     resStr+=i;
+// }
+// return resStr;
+
+
+// }
+// console.log(dublicateStr("Webbrin ccademuyyy"));
